9a073db3c424988ef26a23105970121fa7983b09
kislitsind <72073197+kislitsind@users.noreply.github.com>
2021-04-06 13:30:28 +0600

    Add files via upload

---
+++ №74.py
@@ -0,0 +1,150 @@
+"""
+Имя проекта: №74
+Номер версии: 1.0
+Имя файла: practicum-1(№73).py
+Автор: 2020 © Н.Д.Кислицын, Челябинск
+Лицензия использования:  CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/deed.ru)
+Дата создания: 31/03/2021
+Дата последней модификации: 31/03/2021
+Описание: Создать прямоугольную матрицу A, имеющую N строк и M столбцов со случайными элементами. Определить, сколько отрицательных элементов содержится в каждом столбце и в каждой строке матрицы. Результат оформить в виде матрицы из N + 1 строк и M + 1 столбцов.
+#версия Python: 3.8
+"""
+
+import random
+
+N = 4
+M = 5
+
+
+def get_row(column):
+    col = []
+    for i in range(0, column):
+        col.append(random.randint(-9, 9))
+
+    return col
+
+
+def get_matrix(row, column):
+    matrix = []
+    for i in range(0, row):
+        matrix.append(get_row(column))
+
+    return matrix
+
+
+def print_matrix(matrix):
+    i = 0
+    while i < len(matrix):
+        j = 0
+        row = matrix[i]
+        while j < len(row):
+            column = row[j]
+            print(column, end=' ')
+            j += 1
+
+        print()
+        i += 1
+
+
+A = get_matrix(N, M)
+print("Исходная матрица:")
+print_matrix(A)
+
+new_row = []
+
+i = 0
+while i < len(A):
+    j = 0
+
+    count_row_negative = 0
+    while j < len(A[i]):
+        is_negative = A[i][j] < 0
+
+        if is_negative:
+            count_row_negative += 1
+
+        if len(new_row) <= j:
+            new_row.append(1 if is_negative else 0)
+        else:
+            new_row[j] += 1 if is_negative else 0
+
+        j += 1
+
+    A[i].append(count_row_negative)
+    i += 1
+
+A.append(new_row)
+
+print("Модифицированная матрица:")
+print_matrix(A)
+
+#версия Python: 3.6
+"""
+
+import random
+
+N = 2
+M = 3
+
+
+def get_row(column):
+    col = []
+    for i in range(0, column):
+        col.append(random.randint(-9, 9))
+
+    return col
+
+
+def get_matrix(row, column):
+    matrix = []
+    for i in range(0, row):
+        matrix.append(get_row(column))
+
+    return matrix
+
+
+def print_matrix(matrix):
+    i = 0
+    while i < len(matrix):
+        j = 0
+        row = matrix[i]
+        while j < len(row):
+            column = row[j]
+            print(column, end=' ')
+            j += 1
+
+        print()
+        i += 1
+
+
+A = get_matrix(N, M)
+print("Исходная матрица:")
+print_matrix(A)
+
+new_row = []
+
+i = 0
+while i < len(A):
+    j = 0
+
+    count_row_negative = 0
+    while j < len(A[i]):
+        is_negative = A[i][j] < 0
+
+        if is_negative:
+            count_row_negative += 1
+
+        if len(new_row) <= j:
+            new_row.append(1 if is_negative else 0)
+        else:
+            new_row[j] += 1 if is_negative else 0
+
+        j += 1
+
+    A[i].append(count_row_negative)
+    i += 1
+
+A.append(new_row)
+
+print("Модифицированная матрица:")
+print_matrix(A)
